616622fea72b9b9ae5cd658e910feb46e988734c
David Ramage <david.p.ramage@gmail.com>
2021-09-30 21:17:10 -0700

    add functionality to add a movie

--- web/ui.js
+++ web/ui.js
@@ -1,6 +1,7 @@
-//set Cognito id token outside of scope
+//set vars outside of callback scope
 var id_token = null;
 var movies = null;
+var formats = null;
 var apiUrl = 'https://bxyfauhz4j.execute-api.us-west-2.amazonaws.com/Dev';
 function set_id_token(new_id_token) {
 	id_token = new_id_token;
@@ -8,16 +9,82 @@ function set_id_token(new_id_token) {
 function set_movies(movies_in) {
 	movies = movies_in;
 }
+function set_formats(formats_in) {
+	formats = formats_in;
+}
+
+function create_movie(new_movie_payload) {
+	$.ajax({
+		type: 'POST',
+		url: apiUrl,
+		responseType: 'application/json',
+		contentType: 'application/json',
+		data: JSON.stringify(new_movie_payload),
+		headers: {"Authorization" : id_token},
+		success: function(data) {
+			get_movies();
+		},
+		error: function(data) {
+			console.log("failed to create new movie");
+			$("#c_logonform").dialog("open");
+			console.log(data);
+		}
+	});
+}
+
+function new_movie_openform() {
+	var form_html = '<label for = "name">Movie Name:</label>';
+	form_html += '<input type="text" name="name" id="name">' + "<br>\n";
+	form_html += '<label for = "length">Length:</label>';
+	form_html += '<input type="text" name="length" id="length">' + "<br>\n";
+	form_html += '<label for = "year">Year</label>';
+	form_html += '<input type="text" name="year" id="year">' + "<br>\n";
+	form_html += '<label for = "rating">Rating:</label>';
+	form_html += '<input type = "text" name="rating" id="rating">' + "<br>\n";
+	form_html += '<label for = "format">Format:</label>';
+	form_html += '<select name="format_id" label="format">' + "<br>\n";
+	for (var format of formats) {
+		form_html += '<option value = "' + format.id + '">' + format.format + '</option>' + "\n";
+	}
+	form_html += "</select>\n";
+        form_html += '<input type="submit" value="Create Movie">';
+	$("#newmovieform").html(form_html);
+	$("#c_newmovie").dialog({
+		dialogClass: "no-close"
+	});
+	$("#c_newmovie").dialog("open");
+	$("#newmovieform").submit(function(event) {
+		event.preventDefault();
+		var new_movie_payload = {
+			name: $("#newmovieform").serializeArray()[0].value,
+			length: $("#newmovieform").serializeArray()[1].value,
+			year: $("#newmovieform").serializeArray()[2].value,
+			rating: $("#newmovieform").serializeArray()[3].value,
+			format_id: $("#newmovieform").serializeArray()[4].value
+		};
+		$("#c_newmovie").dialog("close");
+		create_movie(new_movie_payload);
+	});
+}
+
 function draw_movie_table(movies) {
-	console.log(movies);
-	var table_html = '<table id ="movietable">' + "\n";
+	var table_html = '<table id ="movietable" class = "ui-widget">' + "\n";
+	table_html += '<thead class = "ui-widget-header">' + "\n";
 	table_html += '<tr><th>Movie Name</th><th>Length</th><th>Year</th><th>Rating</th><th>Format</th></tr>' + "\n";
+	table_html += "</thead>\n";
+	table_html += '<tbody class = "ui-widget-content">' + "\n";
 	for (var movie of movies) {
 		table_html += '<tr><td>' + movie.name + '</td><td>' + movie.length + '</td><td>' + movie.year + '</td>';
 		table_html += '<td>' + movie.rating + '</td><td>' + movie.format + '</td></tr>' + "\n";
 	}
-	table_html += "</table>"
+	table_html += "</tbody>\n</table>\n"
+	table_html += '<button id ="newmovie" type="button" class="ui-button">Add Movie</button>' + "\n";
 	$("#movietable").html(table_html);
+	$("#newmovie").click(function(event) {
+		console.log("new movie button fires");
+		new_movie_openform();
+	});
+
 }
 
 //pull down movies from API
@@ -29,12 +96,31 @@ function get_movies() {
 		dataType: 'json',
 		headers: {"Authorization" : id_token},
 		success: function(data) {
-			console.log("successfully downloaded movie data");
 			set_movies(JSON.parse(data.body));
 			draw_movie_table(movies);
 		},
 		error: function(data) {
 			console.log("failed to download movie data");
+			$("#c_logonform").dialog("open");
+			console.log(data);
+		}
+	});
+}
+
+
+//pull down formats from API
+function get_formats() {
+	$.ajax({
+		type: 'GET',
+		url: apiUrl + '/formats',
+		responseType: 'application/json',
+		dataType: 'json',
+		headers: {"Authorization" : id_token},
+		success: function(data) {
+			set_formats(JSON.parse(data.body));
+		},
+		error: function(data) {
+			console.log("failed to download format data");
 			$("#c_logonform").dialog("open");
 			console.log(data);
 		}
@@ -49,6 +135,7 @@ $(document).ready(function() {
 	$("#c_logonform").dialog({
 		dialogClass: "no-close"
 	});
+	$("c_logonform").dialog("open");
 	//cognito authentication
 	$("#logon").submit(function(event) {
 		event.preventDefault();
@@ -67,12 +154,13 @@ $(document).ready(function() {
 			onSuccess : function(result) {
 				var token = result.getIdToken().jwtToken;
 				set_id_token(token);
+				get_formats();
 				get_movies();
 			},
 			onFailure : function(error) {
 				console.log("failed to log in");
 				console.log(JSON.stringify(error));
-				$("#_logonform").show();
+				$("#c_logonform").dialog("open");
 			}
 		})
 	});
